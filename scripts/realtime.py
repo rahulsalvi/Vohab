b6a73c6767965e842905b87a5ccea73c4c8d1109
Rahul Salvi <rsalvi006@me.com>
2016-04-30 08:15:46 -0700

    added watchdog script, main function in realtime

--- scripts/realtime.py
+++ scripts/realtime.py
@@ -4,14 +4,14 @@ import audioop
 
 FILENAME_PREFIX = "recordings/audio"
 FILENAME_SUFFIX = ".wav"
-INDEX = 0
 CHUNK = 8192
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 9
+INPUT_DEVICE = 2
 
-def recordFragment(audio, index, chunk, jformat, channels, rate, prefix, suffix, time):
+def recordFragment(audio, stream, index, chunk, jformat, channels, rate, prefix, suffix, time):
     frames = []
     for i in range(0, int(rate / chunk * time)):
         data = stream.read(chunk)
@@ -23,30 +23,36 @@ def recordFragment(audio, index, chunk, jformat, channels, rate, prefix, suffix,
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
 
-p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK,
-                input_device_index=2)
+def main():
+    p = pyaudio.PyAudio()
 
-try:
-    while True:
-        data = stream.read(CHUNK)
-        rms = audioop.rms(data, 2)    # here's where you calculate the volume
-        print(rms)
-        if rms > 2000:
-            print("Recording")
-            recordFragment(p, INDEX, CHUNK, FORMAT, CHANNELS, RATE, FILENAME_PREFIX, FILENAME_SUFFIX, RECORD_SECONDS)
-            INDEX = INDEX + 1
-            print("Finished")
-except KeyboardInterrupt:
-        pass
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK,
+                    input_device_index=INPUT_DEVICE)
 
-print("")
-print("Cleaning up")
-stream.stop_stream()
-stream.close()
-p.terminate()
+    try:
+        index = 0
+        while True:
+            data = stream.read(CHUNK)
+            rms = audioop.rms(data, 2)    # here's where you calculate the volume
+            print(rms)
+            if rms > 2000:
+                print("Recording")
+                recordFragment(p, stream, index, CHUNK, FORMAT, CHANNELS, RATE, FILENAME_PREFIX, FILENAME_SUFFIX, RECORD_SECONDS)
+                index = index + 1
+                print("Finished")
+    except KeyboardInterrupt:
+            pass
+
+    print("")
+    print("Cleaning up")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+if __name__ == "__main__":
+    main()
