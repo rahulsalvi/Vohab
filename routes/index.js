700db6260efef5a1d31bd8f7b5e4b720a69b0471
Laura Gouillon <lgouillon@yahoo.com>
2016-04-30 07:34:29 -0700

    Adding node.js website framework and intial splash and dashboard pages

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { dashboard: 'DASHBOARD CAT' });
+});
+
+router.get('/carousel', function(req, res, next) {
+  res.render('carousel', { dashboard: 'CAROUSEL' });
+});
+
+module.exports = router;
